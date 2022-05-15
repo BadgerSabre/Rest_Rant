70ce8c5d75d4de38de7276d0ce68f8d9bd927cd4
Brandon Dega <brandondega@gmail.com>
2022-05-15 09:45:03 -0500

    refactored index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
+const placesRoutes = require('./controllers/places')
+
 const app = express()
 
-app.use('/places', require('./controllers/places'))
+app.use('/places', placesRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -12,4 +14,6 @@ app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
-app.listen(process.env.PORT)
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {`listening on port ${PORT}`})
