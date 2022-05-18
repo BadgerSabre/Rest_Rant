2a2dc396d252f7a0daabc1c1724b631b2680dcc7
Brandon Dega <brandondega@gmail.com>
2022-05-18 10:31:32 -0500

    styled form

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,31 @@
 const router = require('express').Router()
 
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'https://thumbs.dreamstime.com/b/thai-food-417761.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'https://thumbs.dreamstime.com/b/espresso-coffee-cup-beans-vintage-table-90374872.jpg'
+  }]
+
 // GET /places
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'https://thumbs.dreamstime.com/b/thai-food-417761.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'https://thumbs.dreamstime.com/b/espresso-coffee-cup-beans-vintage-table-90374872.jpg'
-      }]
-      
+router.get('/', (req, res) => {     
     res.render('places/index', { places })
 })
 
 router.get('/new', (req, res) => {
     res.render('places/new')
+})
+
+// POST /places
+router.post('/', (req, res) => {
+  res.send('POST /places')
 })
 
 module.exports = router
