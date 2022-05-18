d986afd61ab64ea075928a87b0ed4d9cb9973d54
Brandon Dega <brandondega@gmail.com>
2022-05-17 20:16:45 -0500

    changed home title

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+              <h1>REST-Rant</h1>
               <div>
                 <img id="toast-photo" src="/images/toast.jpg" alt="Yummy Toast"></img>
                 <div>
