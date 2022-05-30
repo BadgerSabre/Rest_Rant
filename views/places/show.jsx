da073b8ab49281b1b384ab9a3a3dbf1959c94b4c
Brandon Dega <brandondega@gmail.com>
2022-05-30 06:43:17 -0500

    added helper function to model

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,12 +6,16 @@ function Show (data) {
         <Def>
             <main>
                 <h1>{ data.place.name }</h1>
+                <img src={data.place.pic} alt={data.place.name}></img>
                 <h2>Rating</h2>
-                <p>Not Rated</p>
+                    <p>Not Rated</p>
                 <h2>Description</h2>
-                <p>Located in{ data.place.city }, { data.place.state } and serving { data.place.cuisines }</p>
+                    <h4>
+                        {data.place.showEstablished()}
+                    </h4>
+                        <p>Located in{ data.place.city }, { data.place.state } and serving { data.place.cuisines }</p>
                 <h2>Comments</h2>
-                <p>No comments yet!</p>
+                    <p>No comments yet!</p>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit Place
                 </a>
